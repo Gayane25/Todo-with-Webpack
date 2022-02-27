5c2f1bb669bf5392242d6deacb7ec50610629941
Gayane25 <gayabennu@gmail.com>
2022-02-27 22:10:46 +0400

    feat:added plugins

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      // Loading images
+      {
+        test: /\.(png|jpg|jpeg|gif|ico)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images',
+              name: '[name]-[sha1:hash:7].[ext]',
+            },
+          },
+        ],
+      },
+      // Loading fonts
+      {
+        test: /\.(ttf|otf|eot|woff)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts',
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+      // Loading css
+      {
+        test: /\.(css)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles-[hash:8].css',
+    }),
+  ],
+  devServer: {
+    open: true,
+  },
+};
